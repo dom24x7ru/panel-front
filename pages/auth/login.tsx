654b5db0b6cfec26c79cb541b81d0e4116701fc4
Ilya Dergunov <i.dergunov@itq-group.com>
2022-10-27 02:46:08 +0300

    DOMXRU - 91\DOMXRU - 93 Добавлен проект, добавлена авторизация

---
+++ pages/auth/login.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+const Login: NextPage = () => (
+  <div>
+    <LoginForm />
+  </div>
+);
+
+export default Login;
